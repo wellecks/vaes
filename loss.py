51d48c5f51ba1eca004e476333c70d1d34ae37f9
isaachenrion <isaachenrion@gmail.com>
2016-12-07 11:51:50 -0500

    Separated neural net from encoder

---
+++ loss.py
@@ -0,0 +1,51 @@
+
+import tensorflow as tf
+
+def cross_entropy(obs, actual, offset=1e-7):
+    """Binary cross-entropy, per training example"""
+    # (tf.Tensor, tf.Tensor, float) -> tf.Tensor
+    with tf.name_scope("cross_entropy"):
+        # bound by clipping to avoid nan
+        obs_ = tf.clip_by_value(obs, offset, 1 - offset)
+        return -tf.reduce_sum(actual * tf.log(obs_) +
+                              (1 - actual) * tf.log(1 - obs_), 1)
+
+def analytic_kl_divergence(mu, log_std):
+    '''Compute the batched KL divergence between a diagonal Gaussian and a standard Gaussian'''
+    kl = -(0.5*tf.reduce_sum(1.0 + 2 * log_std - tf.square(mu) - tf.exp(2 * log_std), 1))
+    return kl
+
+def gaussian_log_pdf(mu, log_std, z):
+    '''Compute the log probability density of point z under Gaussian with mean mu and
+    diagonal standard deviation exp(log_std)
+    '''
+    return tf.contrib.distributions.MultivariateNormalDiag(
+                mu=mu, diag_stdev=tf.maximum(tf.exp(log_std), 1e-15)).log_pdf(z)
+
+def elbo_loss(pred, actual, var_reg=1, **kwargs):
+    mu = kwargs['mu']
+    log_std = kwargs['log_std']
+    if 'sum_log_detj' not in kwargs:
+        kl = tf.reduce_mean(analytic_kl_divergence(mu, log_std))
+        rec_err = tf.reduce_mean(cross_entropy(pred, actual))
+        loss = kl + rec_err
+        tf.scalar_summary('KL divergence', kl)
+    else:
+        sum_log_detj, z0, zk  = kwargs['sum_log_detj'], kwargs['z0'], kwargs['zk']
+        sum_log_detj = tf.reduce_mean(sum_log_detj)
+        log_q0_z0 = tf.reduce_mean(gaussian_log_pdf(mu, log_std, z0))
+        log_qk_zk = log_q0_z0 - sum_log_detj
+        log_p_zk = tf.reduce_mean(gaussian_log_pdf(tf.zeros_like(mu), tf.ones_like(mu), zk))
+        log_p_x_given_zk = -tf.reduce_mean(cross_entropy(pred, actual))
+        rec_err = -log_p_x_given_zk
+        loss = log_qk_zk - log_p_zk  - log_p_x_given_zk
+        tf.scalar_summary('Sum of log det Jacobians', sum_log_detj)
+        tf.scalar_summary('Log q0(z0)', log_q0_z0)
+        tf.scalar_summary('Log qk(zk)', log_qk_zk)
+        tf.scalar_summary('Log p(zk)', log_p_zk)
+        tf.scalar_summary('Log p(x|zk)', log_p_x_given_zk)
+
+    tf.scalar_summary('ELBO', loss)
+
+    tf.scalar_summary('Reconstruction error', rec_err)
+    return loss
--- models.py
+++ models.py
@@ -1,34 +1,24 @@
 
 import tensorflow as tf
-from utils import *
+from neural_networks import *
 import numpy as np
 import pdb
 ###### ENCODERS
 
-def basic_encoder(dim_x, dims_hidden, dim_z):
-    def _basic_encoder(x, e, dim_x, dims_hidden, dim_z):
+def basic_encoder(neural_net, dim_z):
+    def _basic_encoder(x, e, neural_net, dim_z):
         output_dims_dict = {'mu': dim_z, 'log_std': dim_z}
-        last_hidden = nn(x, dim_x, dims_hidden, 'encoder', act=tf.nn.tanh)
+        last_hidden = neural_net(x)
         outputs = {}
         for key in output_dims_dict:
-            outputs[key] = fc_layer(last_hidden, dims_hidden[-1], output_dims_dict[key], layer_name=key, act=None)
+            outputs[key] = fc_layer(last_hidden, output_dims_dict[key], layer_name=key, act=None)
         mu, log_std = outputs['mu'], outputs['log_std']
         z = mu + tf.exp(log_std) * e
         return outputs, z
-    return lambda x, e: _basic_encoder(x, e, dim_x, dims_hidden, dim_z)
+    return lambda x, e: _basic_encoder(x, e, neural_net, dim_z)
 
-def conv_encoder(in_shape, dim_z, layer_dict, widt):
-    def _conv_encoder(x, e, in_shape, dim_z, layer_dict, width):
-        reshaped = tf.reshape(x, (-1, width, width, 1))
-        output_dims_dict = {'mu': dim_z, 'log_std': dim_z}
-        outputs = cnn(reshaped, in_shape, layer_dict, output_dims_dict, 'encoder', act=tf.nn.tanh, final_act=None)
-        mu, log_std = outputs['mu'], outputs['log_std']
-        z = mu + tf.exp(log_std) * e
-        return outputs, z
-    return lambda x, e: _conv_encoder(x, e, in_shape, dim_z, layer_dict, width)
-
-def nf_encoder(dim_x, dims_hidden, dim_z, flow):
-    def _nf_encoder(x, e, dim_x, dims_hidden, dim_z, flow):
+def nf_encoder(neural_net, dim_z, flow):
+    def _nf_encoder(x, e, neural_net, dim_z, flow):
         def norm_flow(z, us, ws, bs, flow):
             sum_log_detj = 0
             for k in range(flow):
@@ -45,64 +35,65 @@ def nf_encoder(dim_x, dims_hidden, dim_z, flow):
             return z, sum_log_detj
 
         output_dims_dict = {'mu': dim_z, 'log_std': dim_z, 'us': dim_z * flow, 'ws': dim_z * flow, 'bs': dim_z * flow}
-        last_hidden = nn(x, dim_x, dims_hidden, 'encoder', act=tf.nn.tanh)
+        last_hidden = neural_net(x)
         outputs = {}
         for key in output_dims_dict:
-            outputs[key] = fc_layer(last_hidden, dims_hidden[-1], output_dims_dict[key], layer_name=key, act=None)
+            outputs[key] = fc_layer(last_hidden, output_dims_dict[key], layer_name=key, act=None)
 
         mu, log_std, us, ws, bs = outputs['mu'], outputs['log_std'], outputs['us'], outputs['ws'], outputs['bs']
 
         z0 = mu + tf.exp(log_std) * e
+        zk, sum_log_detj = norm_flow(z0, us, ws, bs, flow)
+
         outputs['z0'] = z0 # this is z0, pre-flow
-        z, sum_log_detj = norm_flow(z0, us, ws, bs, flow)
-        outputs['zk'] = z # this is zk, post-flow
+        outputs['zk'] = zk # this is zk, post-flow
         outputs['sum_log_detj'] = sum_log_detj
-        return outputs, z
+        return outputs, zk
 
-    return lambda x, e: _nf_encoder(x, e, dim_x, dims_hidden, dim_z, flow)
+    return lambda x, e: _nf_encoder(x, e, neural_net, dim_z, flow)
 
-def iaf_encoder(dim_x, dims_hidden, dim_z, flow):
-    def _iaf_encoder(x, e, dim_x, dims_hidden, dim_z, flow):
+def iaf_encoder(neural_net, dim_z, flow):
+    def _iaf_encoder(x, e, neural_net, dim_z, flow):
         output_dims_dict = {'mu': dim_z, 'log_std': dim_z, 'flow_mus': dim_z * flow, 'flow_sds': dim_z * flow}
-        last_hidden = nn(x, dim_x, dims_hidden, 'encoder', act=tf.nn.tanh)
+        last_hidden = neural_net(x)
         outputs = {}
         for key in ['mu', 'log_std']:
-            outputs[key] = fc_layer(last_hidden, dims_hidden[-1], output_dims_dict[key], layer_name=key, act=None)
+            outputs[key] = fc_layer(last_hidden, output_dims_dict[key], layer_name=key, act=None)
         for key in ['flow_mus', 'flow_sds']:
-            outputs[key] = made_layer(last_hidden, dims_hidden[-1], output_dims_dict[key], layer_name=key, act=None)
+            outputs[key] = made_layer(last_hidden, output_dims_dict[key], layer_name=key, act=None)
 
         mu, log_std, flow_mus, flow_sds = outputs['mu'], outputs['log_std'], outputs['flow_mus'], outputs['flow_sds']
 
         z0 = mu + tf.exp(log_std) * e # preflow
 
-        z, log_detj = _iaf(z0, flow_mus, flow_sds) # apply the IAF
-        outputs['log_detj'] = log_detj
-        return outputs, z
+        zk, sum_log_detj = _iaf(z0, flow_mus, flow_sds, flow) # apply the IAF
+        outputs['sum_log_detj'] = sum_log_detj
+        outputs['z0'] = z0
+        outputs['zk'] = zk
 
-    def _iaf(z, mu, sd):
+        return outputs, zk
+
+    def _iaf(z, mus, sds, flow):
         # IAF transformation and log det of Jacobian; eqns (9) and (10) of [Kingma 2016]
-        z = (z - mu) #TODO currently mu has shape (dim_z, dim_z) whereas z has shape (bs, dim_z)
-        z /= sd
-        log_detj = -tf.reduce_sum(tf.log(sd), 1)
-        return z, log_detj
+        sum_log_detj = 0
+        for k in range(flow):
+            mu, sd = mus[:, k*dim_z:(k+1)*dim_z], sds[:, k*dim_z:(k+1)*dim_z]
+            z = (z - mu) #TODO currently mu has shape (dim_z, dim_z) whereas z has shape (bs, dim_z)
+            z /= sd
+            sum_log_detj += -tf.reduce_sum(tf.log(sd), 1)
+        return z, sum_log_detj
 
-    return lambda x, e: _iaf_encoder(x, e, dim_x, dims_hidden, dim_z, flow)
+    return lambda x, e: _iaf_encoder(x, e, neural_net, dim_z, flow)
 
 
 ###### DECODERS
-def basic_decoder(dim_x, dims_hidden, dim_z):
-    def _basic_decoder(z, dim_x, dims_hidden, dim_z):
+def basic_decoder(neural_net, dim_x):
+    def _basic_decoder(z, neural_net, dim_x):
         with tf.variable_scope('decoder'):
-            dim_out = dims_hidden[0]
-            h = fc_layer(z, dim_z, dim_out, 'layer0')
-            for i in range(len(dims_hidden) - 1):
-                dim_in = dims_hidden[i]
-                dim_out = dims_hidden[i+1]
-                h = fc_layer(h, dim_in, dim_out, 'layer_{}'.format(i+1))
-            dim_in = dims_hidden[-1]
-            x_pred = fc_layer(h, dim_in, dim_x, 'mu', act=tf.sigmoid)
+            last_hidden = neural_net(z)
+            x_pred = fc_layer(last_hidden, dim_x, 'mu', act=tf.sigmoid)
         return x_pred
-    return lambda z: _basic_decoder(z, dim_x, dims_hidden, dim_z)
+    return lambda z: _basic_decoder(z, neural_net, dim_x)
 
 #####################################
 # DEPRECATED
@@ -121,6 +112,19 @@ def old_encoder(x, e, D, H, Z, initializer=tf.contrib.layers.xavier_initializer)
         log_var = tf.matmul(h, w_v) + b_v
         z = mu + tf.sqrt(tf.exp(log_var))*e
     return mu, log_var, z
+
+
+def old_basic_encoder(dim_x, dims_hidden, dim_z):
+    def _old_basic_encoder(x, e, dim_x, dims_hidden, dim_z):
+        output_dims_dict = {'mu': dim_z, 'log_std': dim_z}
+        last_hidden = nn(x, dim_x, dims_hidden, 'encoder', act=tf.nn.tanh)
+        outputs = {}
+        for key in output_dims_dict:
+            outputs[key] = fc_layer(last_hidden, dims_hidden[-1], output_dims_dict[key], layer_name=key, act=None)
+        mu, log_std = outputs['mu'], outputs['log_std']
+        z = mu + tf.exp(log_std) * e
+        return outputs, z
+    return lambda x, e: _old_basic_encoder(x, e, dim_x, dims_hidden, dim_z)
 
 def old_decoder(z, D, H, Z, initializer=tf.contrib.layers.xavier_initializer):
     with tf.variable_scope('decoder'):
